f70993180fa7314566172c1dbb27dacddf03205d
Benjamin Becker <benj.becker@web.de>
2024-11-08 15:19:30 +0100

    9a - Forms (content)

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -13,3 +13,13 @@ urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowedbooks/', views.LoanedBooksListView.as_view(), name='borrowed-books'),
 ]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
